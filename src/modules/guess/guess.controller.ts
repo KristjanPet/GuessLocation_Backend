ceffa108f3026f791ff66b8972c7e3d46707b9b5
KristjanPet <91966438+KristjanPet@users.noreply.github.com>
2023-05-19 14:27:20 +0200

    Added api for users personal best

--- src/modules/guess/guess.controller.ts
+++ src/modules/guess/guess.controller.ts
@@ -1,9 +1,39 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Inject,
+  Param,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+  forwardRef,
+} from '@nestjs/common'
+import { JwtAuthGuard } from 'modules/auth/guards/jwt.guard'
+import { Public } from 'decorators/public.decorator'
+import { Request, Response } from 'express'
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiTags } from '@nestjs/swagger/dist/index'
+import { PaginatedResult } from 'interfaces/paginated-result.interface'
 import { GuessService } from './guess.service'
+import { Guess } from 'entities/guess.entity'
 
 @ApiTags('Guess')
 @Controller('guess')
 export class GuessController {
   constructor(private readonly guessService: GuessService) {}
+
+  @ApiCreatedResponse({ description: 'List all guesses by user sorted by distance.' })
+  @ApiBadRequestResponse({ description: 'Error getting list of guesses' })
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findByUserId(
+    @Param('id') userId: string,
+    @Query('page') page: number,
+    @Query('take') take: number,
+  ): Promise<PaginatedResult> {
+    return await this.guessService.findByUserId(userId, page, take)
+  }
 }
--- src/modules/guess/guess.service.ts
+++ src/modules/guess/guess.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Guess } from 'entities/guess.entity'
+import { PaginatedResult } from 'interfaces/paginated-result.interface'
+import Logging from 'library/Logging'
 import { AuthService } from 'modules/auth/auth.service'
 import { AbstractService } from 'modules/common/abstract.service'
 import { Repository } from 'typeorm'
@@ -12,5 +14,43 @@ export class GuessService extends AbstractService {
     private readonly authService: AuthService,
   ) {
     super(guessRepository)
+  }
+
+  async findByUserId(userId: string, page: number, take: number): Promise<PaginatedResult> {
+    try {
+      return await this.paginateByAuthor(userId, page, take, ['author', 'guesses'], { distance: 'ASC' })
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException('something went wrong while searching for all locations')
+    }
+  }
+
+  async paginateByAuthor(
+    userId: string,
+    page = 1,
+    take: number,
+    relations = [],
+    order?: Record<string, 'ASC' | 'DESC'>,
+  ): Promise<PaginatedResult> {
+    try {
+      const [data, total] = await this.repository.findAndCount({
+        where: { author: { id: userId } },
+        take,
+        skip: (page - 1) * take,
+        relations,
+        order,
+      })
+
+      return {
+        data: data,
+        meta: {
+          total,
+          page,
+        },
+      }
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException('Something went wrong while searching for a paginated elements.')
+    }
   }
 }
