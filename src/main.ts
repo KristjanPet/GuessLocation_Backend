f9c6fc699aef526cf81afb7f14198751875fe270
KristjanPet <91966438+KristjanPet@users.noreply.github.com>
2023-06-19 14:56:43 +0200

    test 2

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
   })
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: 'https://geotagger.onrender.com',
     credentials: true,
   })
   app.use(cookieParser())
